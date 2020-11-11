364233f06fc75d5e8423b6a83aaf6ee50b2c3f7d
František Žiačik <ziacik@gmail.com>
2020-11-11 22:02:36 +0100

    chore: switched to tabs and reformatted all

--- apps/app/jest.config.js
+++ apps/app/jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  displayName: 'app',
-  preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
-    }
-  },
-  transform: {
-    '^.+\\.[tj]s$':  'ts-jest' 
-  },
-    moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/app'
+	displayName: 'app',
+	preset: '../../jest.preset.js',
+	globals: {
+		'ts-jest': {
+			tsConfig: '<rootDir>/tsconfig.spec.json',
+		},
+	},
+	transform: {
+		'^.+\\.[tj]s$': 'ts-jest',
+	},
+	moduleFileExtensions: ['ts', 'js', 'html'],
+	coverageDirectory: '../../coverage/apps/app',
 };

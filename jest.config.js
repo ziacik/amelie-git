875e895eb342717cf9b3c4fc316793f98756597a
František Žiačik <ziacik@gmail.com>
2022-05-22 00:44:16 +0200

    chore: migration to 13

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
-	projects: ['<rootDir>/apps/web', '<rootDir>/apps/app', '<rootDir>/libs/core', '<rootDir>/libs/testing'],
 	clearMocks: true,
+	projects: getJestProjects(),
 };

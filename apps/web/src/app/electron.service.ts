9f016908d6a5c2966c1d37837890e67867a35e94
František Žiačik <ziacik@gmail.com>
2022-07-17 21:22:10 +0200

    chore(web): electron service removed

--- apps/web/src/app/electron.service.ts
+++ apps/web/src/app/electron.service.ts
@@ -1,20 +1,11 @@
 import { Injectable } from '@angular/core';
-import { from, Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class ElectronService {
-	private electronCached: Electron.RendererInterface | undefined;
-
-	private get electron(): Electron.RendererInterface | undefined {
-		if (!this.electronCached) {
-			this.electronCached = globalThis.require('electron');
-		}
-		return this.electronCached;
-	}
-
 	invoke<T>(channel: string, ...args: unknown[]): Observable<T> {
-		return from(<Promise<T>>this.electron?.ipcRenderer.invoke(channel, ...args));
+		return of(undefined as unknown as T);
 	}
 }

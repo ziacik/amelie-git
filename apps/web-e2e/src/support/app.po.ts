3a8344c017ed16f1cc32f5e8cc65ca679d8455f0
František Žiačik <ziacik@gmail.com>
2022-07-23 19:46:54 +0200

    chore: synchronized configs to newly generated workspace
    Previous migrations didn't do quite a good job. So I've generated a fresh nx workspace, compared the configs and made the changes so that the configs are roughly the same.

--- apps/web-e2e/src/support/app.po.ts
+++ apps/web-e2e/src/support/app.po.ts
@@ -1,2 +1,1 @@
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const getGreeting = () => cy.get('h1');
--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -5,17 +5,15 @@ export default {
 	setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
 	globals: {
 		'ts-jest': {
-			stringifyContentPathRegex: '\\.(html|svg)$',
-
 			tsconfig: '<rootDir>/tsconfig.spec.json',
+			stringifyContentPathRegex: '\\.(html|svg)$',
 		},
 	},
 	coverageDirectory: '../../coverage/apps/web',
-
 	transform: {
-		'^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
+		'^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
 	},
-	transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
+	transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
 	snapshotSerializers: [
 		'jest-preset-angular/build/serializers/no-ng-attributes',
 		'jest-preset-angular/build/serializers/ng-snapshot',

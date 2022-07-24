ac252e836caacfb43fe19d169fcb4b2eb8a9b1e7
František Žiačik <ziacik@gmail.com>
2022-07-24 20:26:59 +0200

    test(ipc): spec reorganization

--- libs/ipc/src/lib/ipc.service.spec.ts
+++ libs/ipc/src/lib/ipc.service.spec.ts
@@ -35,43 +35,49 @@ describe('IpcService', () => {
 		expect(Neutralino.init).toHaveBeenCalled();
 	});
 
-	it('openRepository opens a folder selection dialog and returns the selected one', async () => {
-		const expected = '/selected/path';
-		const actual = await service.openRepository();
-		expect(actual).toEqual(expected);
+	describe('openRepository', () => {
+		it('opens a folder selection dialog and returns the selected one', async () => {
+			const expected = '/selected/path';
+			const actual = await service.openRepository();
+			expect(actual).toEqual(expected);
+		});
 	});
 
-	it('getBranches requests a list of local branches and returns them', async () => {
-		const expected = [new Branch('another'), new Branch('master')];
-		const actual = await service.getBranches(path.resolve(__dirname, '../__fixtures__/repo'));
-		expect(actual).toEqual(expected);
+	describe('getBranches', () => {
+		it('requests a list of local branches and returns them', async () => {
+			const expected = [new Branch('another'), new Branch('master')];
+			const actual = await service.getBranches(path.resolve(__dirname, '../__fixtures__/repo'));
+			expect(actual).toEqual(expected);
+		});
+
+		it('fails if git fails', async () => {
+			await expect(service.getBranches(path.resolve(__dirname, '../__fixtures__'))).rejects.toThrow(
+				`Git error: fatal: not a git repository: '_git'`
+			);
+		});
 	});
 
-	it('getBranches fails if git fails', async () => {
-		await expect(service.getBranches(path.resolve(__dirname, '../__fixtures__'))).rejects.toThrow(
-			`Git error: fatal: not a git repository: '_git'`
-		);
-	});
-
-	it('getLog requests a list of commits and returns them', async () => {
-		const commits = await service.getLog(path.resolve(__dirname, '../__fixtures__/repo'));
-		const commit1 = commits[commits.length - 1];
-		const commit2 = commits[commits.length - 2];
-		expect(commit1.id).toBe('61c4fef6f915862a51acf5395728954834652d4a');
-		expect(commit1.name).toBe('initial commit');
-		expect(commit1.message).toBe('Some description\nabout the initial commit');
-		expect(commit1.author.name).toBe('František Žiačik');
-		expect(commit1.author.email).toBe('ziacik@gmail.com');
-		expect(commit1.committer.name).toBe('František Žiačik');
-		expect(commit1.committer.email).toBe('ziacik@gmail.com');
-		expect(commit1.parentIds).toEqual([]);
-		expect(commit2.id).toBe('f2403434af146c2c5bf5150282b8fd50a4f6a7d9');
-		expect(commit2.name).toBe('second');
-		expect(commit2.message).toBeFalsy();
-		expect(commit2.author.name).toBe('František Žiačik');
-		expect(commit2.author.email).toBe('ziacik@gmail.com');
-		expect(commit2.committer.name).toBe('František Žiačik');
-		expect(commit2.committer.email).toBe('ziacik@gmail.com');
-		expect(commit2.parentIds).toEqual(['61c4fef6f915862a51acf5395728954834652d4a']);
+	describe('getLog', () => {
+		it('getLog requests a list of commits and returns them', async () => {
+			const commits = await service.getLog(path.resolve(__dirname, '../__fixtures__/repo'));
+			const commit1 = commits[commits.length - 1];
+			const commit2 = commits[commits.length - 2];
+			expect(commit1.id).toBe('61c4fef6f915862a51acf5395728954834652d4a');
+			expect(commit1.name).toBe('initial commit');
+			expect(commit1.message).toBe('Some description\nabout the initial commit');
+			expect(commit1.author.name).toBe('František Žiačik');
+			expect(commit1.author.email).toBe('ziacik@gmail.com');
+			expect(commit1.committer.name).toBe('František Žiačik');
+			expect(commit1.committer.email).toBe('ziacik@gmail.com');
+			expect(commit1.parentIds).toEqual([]);
+			expect(commit2.id).toBe('f2403434af146c2c5bf5150282b8fd50a4f6a7d9');
+			expect(commit2.name).toBe('second');
+			expect(commit2.message).toBeFalsy();
+			expect(commit2.author.name).toBe('František Žiačik');
+			expect(commit2.author.email).toBe('ziacik@gmail.com');
+			expect(commit2.committer.name).toBe('František Žiačik');
+			expect(commit2.committer.email).toBe('ziacik@gmail.com');
+			expect(commit2.parentIds).toEqual(['61c4fef6f915862a51acf5395728954834652d4a']);
+		});
 	});
 });
